1606a3097e2763efc7df8288fe9ef556c1fdf0ac
manpoyang <manpoyang2009@163.com>
2024-12-27 21:16:08 +0800

    first

---
+++ app/src/main/java/com/mkt/bocd/point/BocdPointApplication.java
@@ -0,0 +1,18 @@
+package com.mkt.bocd.point;
+
+/**
+ * @author manpoyang
+ * @version 1.0
+ * @date 2024/12/27 19:56
+ */
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("com.mkt.bocd.point.mapper")
+public class BocdPointApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BocdPointApplication.class, args);
+    }
+}
