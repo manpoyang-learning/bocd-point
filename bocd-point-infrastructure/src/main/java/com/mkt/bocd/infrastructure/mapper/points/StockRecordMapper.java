93a7555244a57fcf887a07b77442e93f6ae28486
manpoyang <manpoyang2009@163.com>
2024-12-29 22:47:12 +0800

    积分商城下单-未完成

---
+++ bocd-point-infrastructure/src/main/java/com/mkt/bocd/infrastructure/mapper/points/StockRecordMapper.java
@@ -0,0 +1,12 @@
+package com.mkt.bocd.infrastructure.mapper.points;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mkt.bocd.infrastructure.entity.points.StockRecord;
+
+/**
+ * @author manpoyang
+ * @version 1.0
+ * @date 2024/12/29 22:46
+ */
+public interface StockRecordMapper extends BaseMapper<StockRecord> {
+}
